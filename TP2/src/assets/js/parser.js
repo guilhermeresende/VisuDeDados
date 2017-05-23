f240842350318e17ccec705c490ca029403b3136
guilherme resende borges <guilhermeborges@urucuia.grad>
2017-05-23 14:52:21 -0300

    Nomes corretos

--- TP2/src/assets/js/parser.js
+++ TP2/src/assets/js/parser.js
@@ -1,6 +1,5 @@
 
-
-function parsecsv(filename, func){
+function parsecsv(filename, createflower,resultcountries){
 
 	var rawFile = new XMLHttpRequest();
 	rawFile.open("GET", filename, true);
@@ -9,8 +8,8 @@ function parsecsv(filename, func){
 			if(rawFile.status === 200 || rawFile.status == 0){
 				var allText = rawFile.responseText;
 				var resultdata= d3.csvParse(allText);	
-				//console.log(resultdata);
-				func(resultdata);		
+				console.log(resultcountries);
+				createflower(resultcountries,resultdata);		
 			}
 		}
 	}
@@ -18,19 +17,23 @@ function parsecsv(filename, func){
   	//return resultdata;
  }
 
-function parsejson(filename){	
-
+function parseeverything(filenamejson,filenamecsv,createflower){	
 	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", filename, true);
-	rawFile.onreadystatechange = function (){
+	rawFile.open("GET", filenamejson, true);
+	rawFile.onload = function (){
 		if(rawFile.readyState === 4){
 			if(rawFile.status === 200 || rawFile.status == 0){
 				var allText = rawFile.responseText;
 				var resultcountries=JSON.parse(allText);
-				console.log(resultcountries);
+				parsecsv(filenamecsv,createflower,resultcountries);
 			}
 		}
 	}
 	rawFile.send(null);
-  //return resultcountries;
+}
+
+
+function readjson(text){
+	
+	return resultcountries;
 }
